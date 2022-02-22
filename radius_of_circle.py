9c33ab2a2854eaddc7f8adf064d7421d1d1cde7c
RAKSHITH2224 <99284605+RAKSHITH2224@users.noreply.github.com>
2022-02-22 17:38:25 +0530

    Add files via upload

---
+++ radius_of_circle.py
@@ -0,0 +1,10 @@
+'''''
+Input the radius of the circle: 1.1
+The area of the circle with radius 1.1 is:3.8013271108436504
+'''''
+
+x = float(input("Enter Radius of the Circle:  "))
+from math import pi
+area = pi*x**2
+print("Area of the circle is: ")
+print(area)
